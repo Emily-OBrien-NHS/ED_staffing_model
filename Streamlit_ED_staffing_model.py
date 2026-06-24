a5ca22dcf90e2da332a687f68a59565d04e24d1b
Emily-OBrien-NHS <e.obrien6@nhs.net>
2026-06-24 10:38:09 +0100

    streamlit deploy

--- Streamlit_ED_staffing_model.py
+++ Streamlit_ED_staffing_model.py
@@ -417,7 +417,7 @@ if 'pat' in st.session_state:
         #create check for if input and outted arrivals are different
         demand['warn'] = ((abs(demand['Output'] - demand['Input']) / demand['Input']) > 0.3)
 
-        st.dataframe(pat.groupby(['Area', 'Day', 'Run'], as_index=False)['Patient ID'].count())
+        st.dataframe((pat.groupby(['Day', 'Area'], as_index=False)['Patient ID'].count() / (pat['Run'].max() + 1)))
 
         ####4hr performance
         #overall
